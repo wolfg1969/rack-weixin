9d5cc3e8d289827f10f19e53e357ddb71adfb152
wolfg1969 <wolfg1969@gmail.com>
2013-02-21 03:45:40 +0000

    weixin validation

--- lib/weixin.rb
+++ lib/weixin.rb
@@ -9,7 +9,12 @@ class Weixin
 
     def call(env)
         @req = Rack::Request.new(env)
-        return invalid_request! unless request_valid?
+        return invalid_request! unless request_is_valid?
+        return [
+            200, 
+            { 'Content-type' => 'text/html', 'Content-length' => @req.params['echostr'].length.to_s }, 
+            [ @req.params['echostr'] ]
+        ] if @req.get?
         status, headers, body = @app.call(env)
         [status, headers, body]
     end
@@ -18,10 +23,14 @@ class Weixin
         [401, { 'Content-type' => 'text/html', 'Content-Length' => '0'}, []]
     end
 
-    def request_valid?
-        param_array = [@app_token, @req.params["timestamp"], @req.params["nonce"]]
-        sign = Digest::SHA1.hexdigest( param_array.sort.join )
-        sign == @req.params["signature"] ? true : false
+    def request_is_valid?
+        begin
+            param_array = [@app_token, @req.params['timestamp'], @req.params['nonce']]
+            sign = Digest::SHA1.hexdigest( param_array.sort.join )
+            sign == @req.params['signature'] ? true : false
+        rescue
+            false
+        end
     end
 
 end
