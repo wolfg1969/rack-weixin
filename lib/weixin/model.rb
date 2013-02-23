fb10f6159f973e00b4a8b7950cc2ee5018acc2f5
wolfg1969 <wolfg1969@gmail.com>
2013-02-23 14:04:23 +0000

    message class definition

---
+++ lib/weixin/model.rb
@@ -0,0 +1,36 @@
+module 'Weixin'
+
+    class Message
+        include ROXML
+        
+        xml_name :xml
+        xml_convention :camelcase
+
+        xml_accessor :to_user_name, :cdata => true
+        xml_accessor :from_user_name, :cdata => true
+        xml_accessor :create_time, :as => Integer
+        xml_accessor :msg_type, :cdata => true
+        xml_accessor :msg_id, :as => Integer
+    end
+
+    class TextMessage < Message
+        xml_accessor :content, :cdata => true
+    end
+
+    class ImageMessage < Message
+        xml_accessor :pic_url, :cdata => true
+    end
+
+    class GeoMessage < Message
+        xml_accessor :location_x, :as => Float
+        xml_accessor :location_y, :as => Float
+        xml_accessor :scale, :as => Integer
+        xml_accessor :label, :cdata => true
+    end
+
+    class LinkMessage < Message
+        xml_accessor :title, :cdata => true
+        xml_accessor :description, :cdata => true
+        xml_accessor :url, :cdata => true
+    end
+end
